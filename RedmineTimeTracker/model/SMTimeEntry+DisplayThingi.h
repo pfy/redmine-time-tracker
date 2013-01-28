20f528c5441329f0ef42c09711aeca640481a997
David Gunzinger <david.gunzinger@smooh.ch>
2013-01-28 17:59:13 +0100

    time formatting

---
+++ RedmineTimeTracker/model/SMTimeEntry+DisplayThingi.h
@@ -0,0 +1,14 @@
+//
+//  SMTimeEntry+DisplayThingi.h
+//  RedmineTimeTracker
+//
+//  Created by pfy on 28.01.13.
+//  Copyright (c) 2013 smooh GmbH. All rights reserved.
+//
+
+#import "SMTimeEntry.h"
+
+@interface SMTimeEntry (DisplayThingi)
+-(NSString*)formattedTime;
+
+@end
---
+++ RedmineTimeTracker/model/SMTimeEntry+DisplayThingi.m
@@ -0,0 +1,34 @@
+//
+//  SMTimeEntry+DisplayThingi.m
+//  RedmineTimeTracker
+//
+//  Created by pfy on 28.01.13.
+//  Copyright (c) 2013 smooh GmbH. All rights reserved.
+//
+
+#import "SMTimeEntry+DisplayThingi.h"
+
+@implementation SMTimeEntry (DisplayThingi)
+-(NSString*)formattedTime{
+    double hours = [self.n_hours doubleValue];
+    int hours_int = hours;
+    hours -= hours_int;
+    int minutes_int = 60*hours;
+    hours -= minutes_int / 60.0;
+    
+    int seconds_int = 3600 * hours;
+    
+    return [NSString stringWithFormat:@"%d:%02d:%02d",hours_int,minutes_int,seconds_int ];
+}
+
+-(void)setN_hours:(NSNumber *)n_hours{
+    [self willChangeValueForKey:@"n_hours"];
+    [self willChangeValueForKey:@"formattedTime"];
+    [self setPrimitiveValue:n_hours forKey:@"n_hours"];
+    [self didChangeValueForKey:@"n_hours"];
+    [self didChangeValueForKey:@"formattedTime"];
+    
+
+    
+}
+@end
